16ad00e2e2cdfd1a77e3d3a11f04cf12cbaf0c37
Michael Sutton <michael.k.sutton@gmail.com>
2009-06-03 04:17:47 +0200

    I should probably go to bed... sun is rising. Making progress duplicating the Viddler gem

---
+++ lib/bliptv/api_spec.rb
@@ -0,0 +1,37 @@
+module BlipTV
+  class ApiSpec
+    VIDEOS_UPLOAD_ATTRS = {
+      :required => [
+        :title,
+        :file,
+      ],   
+      :optional => {
+        :thumbnail,
+        :nsfw,
+        :description,
+        :username,
+        :password,
+        :keywords,
+        :categories,
+        :license,
+        :interactive_post
+        }
+    }
+  end
+  
+  def self.check_attributes(bliptv_method, attributes)
+    valid_attributes = bliptv_method_to_const(bliptv_method)
+    required = valid_attributes[:required] || Array.new
+    optional = valid_attributes[:optional] || Array.new
+    
+    attributes.assert_valid_keys(required + optional)
+    attributes.assert_required_keys(required)
+  end
+  
+  protected
+  
+  def self.bliptv_method_to_const(method)
+    const_name = method.gsub('.', '_').upcase
+    const_get("#{const_name}_ATTRS")
+  end
+end
--- lib/bliptv/base.rb
+++ lib/bliptv/base.rb
@@ -17,4 +17,40 @@ module BlipTV
     end
   end
   
+  # Implements the Blip.tv REST Upload API
+  #
+  # <tt>new_attributes</tt> hash should contain next required keys:
+  # * <tt>title:</tt> The video title;
+  # * <tt>file:</tt> The video file;
+  #
+  # and optionally:
+  # * <tt>thumbnail:</tt> A thumbnail file;
+  # * <tt>nsfw:</tt> true if explicit, false otherwise. Defaults to false;
+  # * <tt>description:</tt> A description of the video
+  # * <tt>username:</tt> Username
+  # * <tt>password:</tt> Password
+  # * <tt>keywords:</tt> A comma-separated string of keywords # TODO this should be nice and also accept Arrays
+  # * <tt>categories:</tt> A Hash of categories
+  # * <tt>license:</tt> A license for the video
+  # * <tt>interactive_post:</tt> Specify whether or not a post is interactive. More here[http://wiki.blip.tv/index.php/API_2.0:_Post_Interactivity]
+  #
+  # Example:
+  #
+  #  bliptv.upload_video(:title => 'Check out this guy getting kicked in the nuts!', :file => File.open('/movies/nuts.mov'))
+  #
+  # Returns BlipTV::Video instance. 
+  #
+  def upload_video(new_attributes={})
+    BlipTV::ApiSpec.check_attributes('videos.upload', new_attributes)
+      
+    request = BlipTV::Request.new(:post, 'videos.upload')
+    request.run do |p|
+      for param, value in new_attributes
+        p.send("#{param}=", value)
+      end
+    end
+    BlipTV::Video.new(request.response['video'])
+  end
+  
+  
 end
